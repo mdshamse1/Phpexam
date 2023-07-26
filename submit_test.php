26e7d173cbebb3952832e92fbb2c4ede9bb99f00
mdshamse1 <117460594+mdshamse1@users.noreply.github.com>
2023-07-26 11:48:08 +0530

    Update submit_test.php

--- submit_test.php
+++ submit_test.php
@@ -8,18 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     // Database connection details
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "MYCKBEXAM";
-
-    // Create a new PDO connection with error handling
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
-    }
+    include 'connectDb.php';
 
     // Retrieve the user ID and user type from the session
     $user_id = $_SESSION["user_id"];
@@ -43,56 +32,74 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         exit();
     }
 
-    // Loop through the submitted answers and save them in the database
+    // Retrieve all the questions for the current test
+    $stmt = $conn->prepare("SELECT question_id FROM Question WHERE test_id = :test_id");
+    $stmt->bindParam(":test_id", $test_id);
+    $stmt->execute();
+    $all_question_ids = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+    // Initialize variables to track correct answers and total questions
     $correct_answers = 0;
     $total_questions = 0;
-    foreach ($_POST as $key => $value) {
-        if (strpos($key, "answer_") === 0) {
-            $question_id = substr($key, strlen("answer_"));
-            $selected_option = (int)$value;
 
-            // Validate the selected option (must be an integer between 1 and 4)
-            if (!is_int($selected_option) || $selected_option < 1 || $selected_option > 4) {
-                die("Invalid selected option for question ID: $question_id");
-            }
-
-            // Retrieve the correct option for the current question from the Question table
-            $stmt = $conn->prepare("SELECT correct_option FROM Question WHERE question_id = :question_id AND test_id = :test_id");
-            $stmt->bindParam(":question_id", $question_id);
-            $stmt->bindParam(":test_id", $test_id);
-            $stmt->execute();
-            $question_data = $stmt->fetch(PDO::FETCH_ASSOC);
-            $correct_option = ($question_data && isset($question_data['correct_option'])) ? (int)$question_data['correct_option'] : null;
-
-            // Compare the selected option with the correct option
-            if ($correct_option !== null && $correct_option === $selected_option) {
-                // If the selected option matches the correct option, store it as the correct answer in the Answer table
-                $correct_answers++;
-            }       
-            
-            // Insert the selected option along with the correct option into the database table `Answer` using a prepared statement
-            $stmt = $conn->prepare("INSERT INTO Answer (employee_id, question_id, selected_option, test_id, correct_option) VALUES (:employee_id, :question_id, :selected_option, :test_id, :correct_option)");
-            $stmt->bindParam(":employee_id", $user_id);
-            $stmt->bindParam(":question_id", $question_id);
-            $stmt->bindParam(":selected_option", $selected_option);
-            $stmt->bindParam(":test_id", $test_id);
-            $stmt->bindParam(":correct_option", $correct_option);
-
-            try {
-                $stmt->execute();
-            } catch(PDOException $e) {
-                die("Error while saving the answer for question ID: $question_id");
-            }
-            
-
-            // Increment the total questions count
-            $total_questions++;
+    // Loop through all the questions and set selected options as 0 for unanswered questions
+    foreach ($all_question_ids as $question_id) {
+        if (!isset($_POST["answer_" . $question_id])) {
+            // If the question ID is not found in the POST data, it means the question was not answered
+            $selected_option = 0;
+        } else {
+            // Retrieve the selected option from the POST data for the current question
+            $selected_option = (int)$_POST["answer_" . $question_id];
         }
+
+        // Retrieve the correct option for the current question from the Question table
+        $stmt = $conn->prepare("SELECT correct_option FROM Question WHERE question_id = :question_id AND test_id = :test_id");
+        $stmt->bindParam(":question_id", $question_id);
+        $stmt->bindParam(":test_id", $test_id);
+        $stmt->execute();
+        $question_data = $stmt->fetch(PDO::FETCH_ASSOC);
+        $correct_option = ($question_data && isset($question_data['correct_option'])) ? (int)$question_data['correct_option'] : null;
+
+        // Compare the selected option with the correct option
+        if ($correct_option !== null && $correct_option === $selected_option) {
+            // If the selected option matches the correct option, store it as the correct answer in the Answer table
+            $correct_answers++;
+        }
+
+        // Insert the selected option along with the correct option into the database table `Answer` using a prepared statement
+        $stmt = $conn->prepare("INSERT INTO Answer (employee_id, question_id, selected_option, test_id, correct_option) VALUES (:employee_id, :question_id, :selected_option, :test_id, :correct_option)");
+        $stmt->bindParam(":employee_id", $user_id);
+        $stmt->bindParam(":question_id", $question_id);
+        $stmt->bindParam(":selected_option", $selected_option);
+        $stmt->bindParam(":test_id", $test_id);
+        $stmt->bindParam(":correct_option", $correct_option);
+
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            die("Error while saving the answer for question ID: $question_id");
+        }
+
+        // Increment the total questions count
+        $total_questions++;
     }
 
     // Calculate the marks obtained and the percentage for the test
-    $marks_obtained = $correct_answers;
-    $percentage = ($total_questions > 0) ? (($correct_answers / $total_questions) * 100) : 0;
+    if ($correct_answers === 0) {
+        // If the user has not answered any question correctly, set marks_obtained and percentage to 0
+        $marks_obtained = 0;
+        $percentage = 0;
+    } else {
+        // Calculate marks obtained and percentage only if the user has answered at least one question correctly
+        $marks_obtained = $correct_answers;
+        $percentage = ($total_questions > 0) ? (($correct_answers / $total_questions) * 100) : 0;
+    }
+
+    // Check if the user has answered at least one question
+    if ($total_questions === 0) {
+        // If the user has not answered any question, display an error message
+        die("Error: You must answer at least one question to submit the test.");
+    }
 
     // Insert the test result into the `Result` table along with the date and time of submission
     $stmt = $conn->prepare("INSERT INTO Result (employee_id, test_id, marks_obtained, percentage, submission_datetime) VALUES (:employee_id, :test_id, :marks_obtained, :percentage, NOW())");
@@ -103,7 +110,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     try {
         $stmt->execute();
-    } catch(PDOException $e) {
+    } catch (PDOException $e) {
         die("Error while saving the test result.");
     }
 
@@ -120,7 +127,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 
 // Function to sanitize user inputs
-function sanitize_input($input) {
+function sanitize_input($input)
+{
     return htmlspecialchars(stripslashes(trim($input)));
 }
-?>
